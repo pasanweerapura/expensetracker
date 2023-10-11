73481393e269f4e9d1b076096ae1f3b4e8bad109
pasanweerapura <pasanmenaka123@gmail.com>
2023-10-11 10:10:32 +0530

    first commit

---
+++ WebApplication4/Controllers/ExpenseController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication4.Data;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expenses;
+            return View(objList);
+
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Add(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+
+
+        //Get Delete
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+
+
+                return NotFound();
+
+            }
+
+            var obj = _db.Expenses.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+
+
+            return View(obj);
+
+
+        }
+
+        //Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+
+
+                return NotFound();
+
+            }
+
+            _db.Expenses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+        public IActionResult Update(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+
+
+                return NotFound();
+
+            }
+
+            var obj = _db.Expenses.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+
+
+            return View(obj);
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+    }
+}
---
+++ WebApplication4/Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApplication4.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public String Borrower { get; set; }
+        public String Lender { get; set; }
+
+        [DisplayName("Item Name")]  
+        public String ItemName { get; set; }
+        
+
+    }
+}
